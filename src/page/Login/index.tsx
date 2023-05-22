95f96749d4e6eb017aaaab47329ee164688f4427
윤현지 <hyoon@student.42seoul.kr>
2023-05-22 21:17:59 +0900

    :lipstick: Update UI

--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -1,8 +1,10 @@
 
 const Login = () => {
   return (
-    <div className="text-lg w-10">
-      login
+    <div className="px-4 flex">
+      <span>sdfksdflkj</span>
+      <span>login</span>
+      sdlkfjlskdf
     </div>
   );
 };

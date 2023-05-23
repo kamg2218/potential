88f98ba57e22eb288ce3d404e266066614d57efc
윤현지 <hyoon@student.42seoul.kr>
2023-05-23 23:32:31 +0900

    :lipstick: Responsive Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,19 @@
 import { Outlet } from "react-router-dom";
 
-import Header from "./header/Header";
-import Footer from "./footer/Footer";
+// import Header from "./header/Header";
+// import Footer from "./footer/Footer";
 
 const Layout = () => {
   return (
-    <div className='h-screen w-screen flex'>
-      <div className=""></div>
-      <div className="block bg-sky-100 max-w-lg w-full">
-        <Header />
+    <div className='h-screen w-screen flex justify-start lg:justify-center'>
+      <div className="w-0 sm:w-20 lg:w-0"></div>
+      <div className="block bg-sky-100 max-w-xl w-full h-full">
+        {/* <Header /> */}
         <main>
           <Outlet />
         </main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
-      <div className='sm:w-0 w-1/2'></div>
     </div>
   );
 };

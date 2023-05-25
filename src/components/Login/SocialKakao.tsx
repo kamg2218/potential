a3465a83551fac56dccda7daf437c782020e23d0
jungjinbeom <insunok0715@naver.com>
2023-05-26 01:46:07 +0900

    kakao key edit

--- src/components/Login/SocialKakao.tsx
+++ src/components/Login/SocialKakao.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 const SocialKakao = () => {
-  const Rest_api_key = "84e3b52ab109360d6443565c365472dc"; //REST API KEY
-  const redirect_uri = "http://potential.vercel.app/auth"; //Redirect URI
+  const Rest_api_key = "84e3b52ab109360d6443565c365472dc"; //REST API KEY cddf66394b31736dadc6c286c6eb6e5d
+  const redirect_uri = "https://potential.vercel.app/auth"; //Redirect URI http://localhost:5173/auth
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
   const handleLogin = () => {

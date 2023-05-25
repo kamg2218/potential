804a02687bf584ac44309d071b5de2cd3153c0c0
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2023-05-26 02:22:20 +0900

    사용하지 않는 import 문 삭제 (#11)
    * import delete
    
    * import delete

--- src/components/Login/Oauth.tsx
+++ src/components/Login/Oauth.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 export default function Oauth() {
   const navigate = useNavigate();
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,5 +1,3 @@
-import Button from "../Button";
-
 import styled from "styled-components";
 
 import SocialKakao from "./SocialKakao";
@@ -26,20 +24,6 @@ const Wrapper = styled.div`
   margin-bottom: 15rem;
   h1 {
     margin-bottom: 4rem;
-  }
-`;
-const StyledButton = styled(Button)`
-  width: 60%;
-  height: fit-content;
-  border: 1px solid lightgrey;
-  border-radius: 8px;
-  padding: 1rem 2rem;
-
-  color: grey;
-
-  :hover {
-    color: black;
-    border: 1px solid black;
   }
 `;
 
